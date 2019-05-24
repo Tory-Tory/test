e0000cd18d7cd4d578b2d764d06011585e395775
Виктория <v.naumova@digital-sector.ru>
2019-05-24 03:02:02 +0300

    users autorize

--- classes/helper.php
+++ classes/helper.php
@@ -8,17 +8,4 @@ class Helper
         print_r($arr);
         echo '</pre>';
     }
-
-    public function isAutorize($data){
-        if(!empty($data)){
-
-            if ($user){
-                return true;
-            } else {
-                return false;
-            }
-        }else{
-            return false;
-        }
-    }
 }
